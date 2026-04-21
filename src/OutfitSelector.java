0cb4ad6699fceafa4ec4d0a8fa75e0ea92e7f6bf
Joselyn Quinteros <joselynquint13@gmail.com>
2026-04-20 17:39:45 -0700

    Interface

---
+++ src/OutfitSelector.java
@@ -0,0 +1,12 @@
+/**
+ * Outfit Selector Interface
+ * 
+ * Includes the core functionality for an outfit selector system.
+ * 
+ * Supports clothing storage, filtering, outfit generation, 
+ * wear tracking, compatibility checks, and scoring
+ */
+public interface OutfitSelector{
+
+    
+}
