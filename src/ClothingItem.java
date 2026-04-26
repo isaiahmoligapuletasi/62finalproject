b0c0810c700d1009ebc63447ea16c5ec56029297
Joselyn Quinteros <joselynquint13@gmail.com>
2026-04-26 13:33:19 -0700

    Clothing Item

--- src/ClothingItem.java
+++ src/ClothingItem.java
@@ -1,3 +1,22 @@
 public class ClothingItem {
+    //Label of the item like "Long Sleeve"
+    private String name;
 
+    //Labels like "Top" or "Bottom"
+    private String category; 
+
+    private String color;
+    //Labels like "Cotton", "Denim", etc
+    private String texture;
+
+    //Labels like "Casual", "Atheletic wear"
+    private String style;
+
+    public ClothingItem(String name, String category, String color, String texture, String style) {
+        this.name = name;
+        this.category = category;
+        this.color = color;
+        this.texture = texture;
+        this.style = style;
+    }
 }
