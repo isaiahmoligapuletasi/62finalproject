20b8a2d82974fb0ca36bc726cb43c0ed863558fe
Isaiah Moliga-Puletasi <ismq2025@mymail.pomona.edu>
2026-05-07 14:42:57 -0700

    "2:42"

--- src/ClothingSorterAlgorithm.java
+++
@@ -1,9 +0,0 @@
-public class ClothingSorterAlgorithm {
-
-    // Build out an ArrayList of clothing item 
-    // This is the part where we're figuring out how to parse through the Clothing ArrayList 
-
-
-
-
-}
